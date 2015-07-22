6be23300d183677631f1532a8360bc966307e26d
martmwangi <luthermwangi2@gmail.com>
2015-07-22 20:57:58 +0300

    files added

---
+++ sum.rb
@@ -0,0 +1,31 @@
+math = 345
+eng = 90
+kis = 35
+his = 76
+geo = 82
+
+
+sum = math + eng +kis + his + geo
+puts sum
+
+ if
+     sum % 2 == 0
+     puts "It is even"
+else
+    puts "Not even"
+end
+if
+    sum <= 299
+    puts "Failed"
+elsif
+    sum >=300 && sum<=319
+    print "D+"
+elsif sum >=320 && sum<=349
+    puts "C+"
+elsif sum >=350 && sum<=379
+    print "B+"
+elsif sum >=380 && sum<=399
+    print "A-"
+elsif sum >=400
+     print "A"
+end
